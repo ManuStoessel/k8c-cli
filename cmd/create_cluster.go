085622f1ed0df1f311663c4efd882961a48e6519
Manuel Stößel <manuel@loodse.com>
2020-09-28 16:15:06 +0200

    Fix some license info

--- cmd/create_cluster.go
+++ cmd/create_cluster.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2020 Manuel Stößel <manuel@soessel.dev>
+Copyright © 2020 Manuel Stößel <manuel@stoessel.dev>
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
